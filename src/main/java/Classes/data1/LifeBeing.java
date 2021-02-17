02d6d078aefb5173e0bfec3b6184d5f1cf59084d
Georgie <gbenevento@cifpfbmoll.eu>
2021-02-17 12:54:05 +0100

    Created main attributes and their getters and setters in LifeBeing Class

--- src/main/java/Classes/data1/LifeBeing.java
+++ src/main/java/Classes/data1/LifeBeing.java
@@ -1,4 +1,49 @@
 package Classes.data1;
 
 public abstract class LifeBeing {
+    private String name;
+    private Double height;
+    private Double weight;
+    private Gender gender;
+    private Integer level;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
 }
