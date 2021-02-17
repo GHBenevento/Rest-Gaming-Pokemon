96a820288b815c056443933fc92b7e9a4513b6ff
Pau Segovia <psegovia@cifpfbmoll.eu>
2021-02-18 00:39:38 +0100

    We created enum Action with its 5 actions

--- src/main/java/Classes/data2/Action.java
+++ src/main/java/Classes/data2/Action.java
@@ -1,4 +1,33 @@
 package Classes.data2;
 
 public enum Action {
+    EAT, FIGHT, TRAIN, PLAY ,SLEEP
 }
+/*
+give_food
+    -> -hunger
+    -> -dogecoin
+fight
+    -> -energy
+    -> +hunger
+
+        if win
+             -> +happy
+             -> +dogecoin +bonus
+        else
+            -> -happy
+            -> +dogecoin
+
+train
+    -> +hunger and obtain xp
+    -> +energy +bonus
+    -> -dogecoin
+play
+    -> +happy
+    -> -energy
+sleep
+    -> +energy
+
+ A w->3__+1
+ B l->1__+1
+   */
