10ab4ff5fac4ddd283eca434c96e9f5bdde04126
Georgie <gbenevento@cifpfbmoll.eu>
2021-02-18 17:38:18 +0100

    Modified Actions, added Unittesting Application

--- src/main/java/unittesting/Classes/data2/Action.java
+++ src/main/java/unittesting/Classes/data2/Action.java
@@ -1,33 +1,5 @@
 package unittesting.Classes.data2;
 
 public enum Action {
-    EAT, FIGHT, TRAIN, PLAY ,SLEEP
+    EAT, TRAIN, SLEEP
 }
-/*
-give_food
-    -> -hunger
-    -> -dogecoin
-fight
-    -> -energy
-    -> +hunger
-
-        if win
-             -> +happy
-             -> +dogecoin +bonus
-        else
-            -> -happy
-            -> +dogecoin
-
-train
-    -> +hunger and obtain xp
-    -> +energy +bonus
-    -> -dogecoin
-play
-    -> +happy
-    -> -energy
-sleep
-    -> +energy
-
- A w->3__+1
- B l->1__+1
-   */
---
+++ src/test/java/unittesting/UnittestingApplicationTest.java
@@ -0,0 +1,7 @@
+package unittesting;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UnittestingApplicationTest {
+
+}
