88379c1a16ed27513109cda8dd7bb25ad8113682
Georgie <gbenevento@cifpfbmoll.eu>
2021-02-19 22:07:33 +0100

    Deleted wrong methods

--- src/main/java/Classes/data2/IActions.java
+++ src/main/java/Classes/data2/IActions.java
@@ -1,22 +1,5 @@
 package Classes.data2;
 
 public interface IActions {
-    public static void doEAT() {
-    }
 
-    public static void doFIGHT() {
-    }
-
-    public static void doTRAIN() {
-    }
-
-    public static void doPLAY() {
-    }
-
-    public static void doSLEEP() {
-    }
-
-    public static list getCurrentActions() {
-        return list;
-    }
 }
